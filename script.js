7d8212d2dd5e9cd9e5e692b6851628def1e82d4b
Maksim Pavlov <pavlovmark100@gmail.com>
2020-03-11 22:51:08 +0400

    feat: add interactive for Header and start carousel

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 const MENU = document.getElementById('menu');
+
+MENU.addEventListener('click', (event) => {
+    let itemsMenu = MENU.querySelectorAll('a');
+    let targ = event.target;
+    itemsMenu.forEach(elem => elem.classList.remove('menu_active'));
+    targ.classList.add('menu_active');
+});
