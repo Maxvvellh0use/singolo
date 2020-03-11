6644b29a076f9b02a15e4221aa1a37adcd247c7d
Maksim Pavlov <pavlovmark100@gmail.com>
2020-03-12 03:24:30 +0400

    feat: add carousel

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 const MENU = document.getElementById('menu');
+const CAROUSEL_LEFT = document.getElementById('chev_left');
+const CAROUSEL_RIGHT = document.getElementById('chev_right');
+const CAROUSEL = document.getElementById('carousel');
+const BACKGROUND_CAROUSEL = document.getElementById('phone-section-color');
 
 MENU.addEventListener('click', (event) => {
     let itemsMenu = MENU.querySelectorAll('a');
@@ -6,3 +10,46 @@ MENU.addEventListener('click', (event) => {
     itemsMenu.forEach(elem => elem.classList.remove('menu_active'));
     targ.classList.add('menu_active');
 });
+
+let clicks = 0;
+let transformValue = 0;
+CAROUSEL_LEFT.addEventListener('click', function carouselTranslate() {
+    if (clicks >= 1) {
+        transformValue = 0;
+        CAROUSEL.style.transform =`translateX(${transformValue}px)`;
+        BACKGROUND_CAROUSEL.style.backgroundColor = `#F06C64`;
+        clicks = 0;
+    }
+    else {
+        transformValue += 1241;
+        CAROUSEL.style.transform =`translateX(${transformValue}px)`;
+        BACKGROUND_CAROUSEL.style.backgroundColor = `#648BF0`;
+        clicks++;
+    }
+});
+
+CAROUSEL_RIGHT.addEventListener('click', function carouselTranslate() {
+    if (clicks >= 1) {
+        transformValue = 0;
+        CAROUSEL.style.transform =`translateX(${transformValue}px)`;
+        BACKGROUND_CAROUSEL.style.backgroundColor = `#F06C64`;
+        clicks = 0;
+    }
+    else {
+        transformValue -= 1241;
+        CAROUSEL.style.transform =`translateX(${transformValue}px)`;
+        BACKGROUND_CAROUSEL.style.backgroundColor = `#648BF0`;
+        clicks++;
+    }
+});
+
+console.log(transformValue);
+
+
+
+
+
+
+
+
+
